cea8458822c130c7f163ac11a6a7fb70dede9b48
shizhun <shizhun@genomics.cn>
2020-03-28 20:31:09 +0800

    update scripts

--- bin/FileNumberStat.py
+++ bin/FileNumberStat.py
@@ -9,16 +9,19 @@ def ArgumentParser():
         description='Statistic file number in a given directory and its subdirectories')
     parser.add_argument("--dir","-d",  help='give a directory', required=True)
     parser.add_argument("--out","-o",help="output name")
-    parser.add_argument("--maxdepth","-m",help="max depth directory query")
+    parser.add_argument("--maxdepth","-m",default=999,type=int,help="max depth directory query")
     args = parser.parse_args()
     return args
 
 def DirectoryParser(path,maxdepth):
     out = defaultdict(lambda : 0)
     for root, dirs, files in os.walk(path):
+        n_depth = len(root.split(os.sep))
+        if n_depth > maxdepth: continue
         nFiles = len(files)
         out[root] += nFiles
         out[path] += nFiles
+        print(f"{root}\t{nFiles}")
     return out
 
 def output(dat, file_name):
@@ -30,7 +33,7 @@ def main():
     args = ArgumentParser()
     
     data = DirectoryParser(args.dir,args.maxdepth)
-    output(data,args.output)
+    output(data,args.out)
 
 if __name__ == '__main__':
     main()
